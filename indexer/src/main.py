c6c4c5115146fc9a4ada9122e05ba2bf6cded1f6
1nsidewill <yiguha@gmail.com>
2025-05-21 09:53:41 +0900

    set up for indexer

---
+++ indexer/src/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import logging
+from src.config import get_settings
+from src.api.router import api_router
+from src.auth.router import router as auth_router
+import os
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)  # 로그 레벨 설정
+
+config = get_settings()
+
+# Determine the environment
+environment = os.getenv('ENVIRONMENT', 'dev')
+
+# Initiate app with conditional documentation
+if environment in ['dev']:
+    app = FastAPI(title=config.app_name, root_path="/indexer")  # Enable docs in 'dev' and 'loc'
+else:
+    app = FastAPI(
+        title=config.app_name,
+        root_path="/indexer"
+    )
+
+@app.get("/", include_in_schema=False)
+async def root_redirect():
+    return RedirectResponse(url="/indexer/docs")
+
+# Serve static files
+# app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+# Including Routers
+app.include_router(api_router, prefix="/api", tags=["api"])
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+
+@app.get("/health", include_in_schema=False)
+async def health_check():
+    """
+    간단한 헬스체크 엔드포인트
+    """
+    return {"status": "Good"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, proxy_headers=True, forwarded_allow_ips="*")
