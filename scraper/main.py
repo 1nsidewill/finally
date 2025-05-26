5f915e21890c9909872de760f859b1638ed66562
SidNamo <haheehong3@naver.com>
2025-05-27 00:33:53 +0900

    Update main.py

--- scraper/main.py
+++ scraper/main.py
@@ -5,8 +5,8 @@ from routers import sync
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    #async with engine.begin() as conn:
+        #await conn.run_sync(Base.metadata.create_all)
     yield
 
 app = FastAPI(lifespan=lifespan)
