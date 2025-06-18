040d84596ef4257dc588d1cd24b7aa27e7a506c7
1nsidewill <yiguha@gmail.com>
2025-06-18 09:15:12 +0900

    ee

---
+++ scraper_v2/core/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = quote_plus(os.getenv("POSTGRES_PASSWORD"))
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}/{POSTGRES_DB}"
+    #f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=True,
+    pool_recycle=180,        # 180초마다 커넥션 재사용 방지
+    pool_pre_ping=True       # 커넥션 살아있는지 ping 확인
+)
+
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
